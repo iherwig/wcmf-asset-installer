96b19d07d7e8314b4d492230ad965fab090e8e7b
ingo <ingo@wemove.com>
2014-06-24 16:52:46 +0200

    fixed default path retrieval

--- src/wcmf/Composer/AssetInstaller.php
+++ src/wcmf/Composer/AssetInstaller.php
@@ -1,29 +1,36 @@
 <?php
 namespace wcmf\Composer;
 
+use Composer\Composer;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\IO\IOInterface;
 
 class AssetInstaller extends LibraryInstaller {
 
   const PACKAGE_TYPE = 'wcmf-asset';
   const DEFAULT_INSTALL_DIR = 'app/public/vendor/';
-
-  public function getInstallPath(PackageInterface $package) {
-    $basePath = self::DEFAULT_INSTALL_DIR;
   
-    // check package specific installation path
+  private $defaultInstallDir;
+
+  public function __construct(IOInterface $io, Composer $composer, $type=self::PACKAGE_TYPE) {
+    parent::__construct($io, $composer, $type);
+
+    // check for default installation path
+    $config = $composer->getConfig();
+    $this->defaultInstallDir = $config->has('wcmf-asset-install-dir') ? 
+      $config->get('wcmf-asset-install-dir') : self::DEFAULT_INSTALL_DIR;
+  }
+    
+  public function getInstallPath(PackageInterface $package) {
+    $basePath = $this->defaultInstallDir;
+  
+    // check for package specific installation path
     $extra = $package->getExtra();
     if (isset($extra['install-dir'])) {
       $basePath = $extra['install-dir'];
     }
-    // check root installation path
-    else {
-      $config = $composer->getConfig();
-      if ($config->has('wcmf-asset-install-dir')) {
-        $basePath = $config->get('wcmf-asset-install-dir');
-      }
-    }
+
     // add trailing slash if missing
     if (!preg_match('/\/$/', $basePath)) {
       $basePath .= '/';
